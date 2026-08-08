4875a633603a44cbf52b74161f35d377b9ae4618
Almiex <30891220+Almiex@users.noreply.github.com>
2026-08-08 16:04:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -981,7 +981,7 @@ def main():
         else:
             # Индекс текущей даты для навигации по дням
             if 'hourly_date_index' not in st.session_state:
-                st.session_state.hourly_date_index = len(all_dates_full) - 1
+                st.session_state.hourly_date_index = 0
 
             # Защита от выхода за границы после загрузки другого файла
             if all_dates_full:
@@ -993,14 +993,17 @@ def main():
                     )
                 )
 
-                selected_date = all_dates_full[
-                    st.session_state.hourly_date_index
-                ]
-            else:
-                selected_date = None
+        selected_date = st.selectbox(
+            "Дата:",
+            all_dates_full,
+            index=st.session_state.hourly_date_index
+        )
 
-            selected_dates = []
-            date_range_label = selected_date
+        # синхронизируем индекс с выбранной датой
+        st.session_state.hourly_date_index = all_dates_full.index(selected_date)
+
+        selected_dates = []
+        date_range_label = selected_date
 
 
         st.divider()
